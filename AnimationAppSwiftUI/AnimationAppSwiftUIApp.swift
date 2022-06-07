81ad0052a18772fec95f9cd13017492503e8fe47
Miwka <97275239+albertu4@users.noreply.github.com>
2022-06-08 06:19:22 +0900

    Initial Commit

---
+++ AnimationAppSwiftUI/AnimationAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationAppSwiftUIApp.swift
+//  AnimationAppSwiftUI
+//
+//  Created by Михаил Иванов on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
